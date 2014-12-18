d6a532da6d059aa0b68bc89f5cac5facdb62f620
Daniel Farrell <dfarrell@redhat.com>
2014-12-18 17:48:56 -0500

    Sets up system, downloads and unpacks correctly
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

--- build.sh
+++ build.sh
@@ -1,10 +1,16 @@
 #!/usr/bin/env sh
 
 # Setup system
-sudo yum install -y @development-tools fedora-packager
-usermod -a -G mock $USER
+sudo yum install -y @development-tools fedora-packager wget java-1.7.0-openjdk
+sudo usermod -a -G mock $USER
 
 # Configure rpmbuild dir
 rpmdev-setuptree
-wget -P $HOME/rpmbuild/SOURCES https://nexus.opendaylight.org/content/groups/public/org/opendaylight/integration/distribution-karaf/0.2.1-Helium-SR1/distribution-karaf-0.2.1-Helium-SR1.tar.gz
+if ! -f $HOME/rpmbuild/SOURCES/distribution-karaf-0.2.1-Helium-SR1.tar.gz; then
+    curl -o $HOME/rpmbuild/SOURCES/distribution-karaf-0.2.1-Helium-SR1.tar.gz https://nexus.opendaylight.org/content/groups/public/org/opendaylight/integration/distribution-karaf/0.2.1-Helium-SR1/distribution-karaf-0.2.1-Helium-SR1.tar.gz
+fi
 cp opendaylight.spec $HOME/rpmbuild/SPECS
+
+# Build
+cd $HOME/rpmbuild/SPECS
+rpmbuild -ba opendaylight.spec
--- opendaylight.spec
+++ opendaylight.spec
@@ -7,33 +7,25 @@ Group:		Applications/Communications
 License:	EPL-1.0
 URL:		http://www.opendaylight.org
 BuildArch:  noarch
-Source0:	https://github.com/opendaylight/integration/archive/release/helium-sr1.tar.gz
+Source0:	https://nexus.opendaylight.org/content/groups/public/org/opendaylight/integration/distribution-karaf/0.2.1-Helium-SR1/distribution-karaf-0.2.1-Helium-SR1.tar.gz
 
-BuildRequires:	java-devel
-BuildRequires:	maven
 Requires:       java >= 1:1.7.0
 
 %description
 OpenDaylight Helium SR1 (0.2.1)
 
 %prep
-# TODO: Should be able to use %autosetup -n <name> to get proper name, vs work described in README
-%setup -q
+%autosetup -n distribution-karaf-0.2.1-Helium-SR1
 
 
 %build
-export MAVEN_OPTS="-Xmx1024m -XX:MaxPermSize=256m" && mvn clean install -Dmaven.test.skip=true
 
 
 %install
-# TODO
 
 
 %files
-# TODO
-#%doc
 
 
 
 %changelog
-# TODO
