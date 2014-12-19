7b18107593f23ddcce4a4732b88f772e247ce113
Daniel Farrell <dfarrell@redhat.com>
2014-12-19 15:47:13 -0500

    Basic install script
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

---
+++ install.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+# Update if needed
+rpm_path="$HOME/rpmbuild/RPMS/noarch/opendaylight-0.2.1-1.fc20.noarch.rpm"
+
+# Install software required by ODL
+sudo yum install -y java-1.7.0-openjdk
+
+sudo rpm -i $rpm_path
+echo "You'll need to add /opt/opendaylight-0.2.1/bin to your path:"
+echo "\texport PATH=\$PATH:/opt/opendaylight-0.2.1/bin"
