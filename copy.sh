2410d38edeb43eb1aea2bc3564955e26c9b615a0
Daniel Farrell <dfarrell@redhat.com>
2015-02-10 15:54:59 -0500

    Removed unused (and bug-prone) .service file
    Signed-off-by: Daniel Farrell <dfarrell@redhat.com>

--- copy.sh
+++ copy.sh
@@ -6,6 +6,5 @@ HOST=rpm
 rsync build.sh $HOST:/home/fedora/
 rsync connect.sh $HOST:/home/fedora/
 rsync install.sh $HOST:/home/fedora/
-rsync opendaylight.service $HOST:/home/fedora/
 rsync opendaylight.spec $HOST:/home/fedora/
 rsync uninstall.sh $HOST:/home/fedora/
